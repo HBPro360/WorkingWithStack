ffddd7863524aa034deec3961fe405c5638ed33a
HBPro360 <jphamm360@gmail.com>
2016-05-24 20:56:23 -0500

    WorkingWithStack

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Stack<int> nums = new Stack<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                int num = rnd.Next(0, 1000);
+                nums.Push(num);
+                Console.WriteLine(i.ToString() + " " + num);
+            }
+            Console.WriteLine();
+            for(int i=0; i < 10; i++)
+            {
+                int num = nums.Pop();
+                Console.WriteLine(i.ToString() + " " + num);
+            }
+
+            Console.WriteLine("Press Enter to continue.....");
+            Console.ReadLine();
+
+        }
+    }
+}
